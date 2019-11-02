50f57bef5565c12a95167a90f0f350a5db2da98b
123andrew12 <liandrew2003@gmail.com>
2019-11-02 11:07:04 -0400

    Created a baseline autonomous method that allows us to strafe to the alliance line (Unfinished). Created the autonomous Hardware Map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/BoxHAutonomousHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/BoxHAutonomousHardwareMap.java
@@ -5,12 +5,31 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public abstract class BoxHAutonomousHardwareMap extends LinearOpMode {
     public DcMotor leftFront;
     public DcMotor rightFront;
     public DcMotor leftBack;
     public DcMotor rightBack;
+    private ElapsedTime runtime = new ElapsedTime();
+    static final double     COUNTS_PER_MOTOR_REV_NEVEREST40    = 1120 ;    // eg: NEVEREST 40 Motor Encoder https://www.servocity.com/neverest-40-gearmotor
+    static final double     COUNTS_PER_MOTOR_REV_NEVEREST20    = 560 ;
+    static final double     ROTATIONS_PER_MINUTE    = 160 ;
+    static final double     DRIVE_GEAR_REDUCTION    = 1 ;     // This is < 1.0 if geared UP
+    //MUST BE REMEASURED BEFORE USE. DELETE TELEMETRY AND STUFF ONCE FIXED
+    static final double     WHEEL_DIAMETER_INCHES   = 5 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV_NEVEREST20
+            * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    //Threshold for Gyro turning so that we will not continuously attempt to reach an exact value
+    static final double THRESHOLD = 1.5;
     BNO055IMU imu;
 
     public void init(HardwareMap hardwareMap){
@@ -36,6 +55,26 @@ public abstract class BoxHAutonomousHardwareMap extends LinearOpMode {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         imu.initialize(parameters);
+    }
+    public void gyroTurn (double power, double target)
+    {
+        Orientation angles;
+        while(opModeIsActive()) {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("Heading",angles.firstAngle+" degrees");
+            if(angles.firstAngle < target - THRESHOLD) {
+                telemetry.addData("Status","Turning Left");
+                turnLeft(power);
+            } else if(angles.firstAngle > target + THRESHOLD) {
+                telemetry.addData("Status","Turning Right");
+                turnRight(power);
+            } else {
+                stopMotors();
+                break;
+            }
+            telemetry.update();
+            idle();
+        }
     }
 
     public void goForward(double power) {
@@ -81,6 +120,72 @@ public abstract class BoxHAutonomousHardwareMap extends LinearOpMode {
         rightFront.setPower(0);
         leftBack.setPower(0);
         rightBack.setPower(0);
+    }
+
+    public void encoderDrive(double speed, double leftInches, double rightInches, double leftBackInches, double rightBackInches, double timeoutS) {
+        telemetry.addData("CAUTION", "YOU'RE ILLEGAL, ENSURE THAT THE WHEEL HAS BEEN REMEASURED");
+        boolean quit = true;
+        if (quit)
+        {
+            return;
+        }
+        int newLeftTarget;
+        int newRightTarget;
+        int newLeftBackTarget;
+        int newRightBackTarget;
+
+        if (opModeIsActive()) {
+            newLeftTarget = leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeftBackTarget = leftBack.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightBackTarget = rightBack.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftFront.setTargetPosition(newLeftTarget);
+            rightFront.setTargetPosition(newRightTarget);
+            leftBack.setTargetPosition(newLeftBackTarget);
+            rightBack.setTargetPosition(newRightBackTarget);
+
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftFront.setPower(Math.abs(speed));
+            rightFront.setPower(Math.abs(speed));
+            leftBack.setPower(Math.abs(speed));
+            rightBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Going to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Currently at %7d :%7d",
+                        leftFront.getCurrentPosition(),
+                        rightFront.getCurrentPosition(),
+                        leftBack.getCurrentPosition(),
+                        rightBack.getCurrentPosition()
+                );
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            leftBack.setPower(0);
+            rightBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/BoxHStrafeToLineRed.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.teleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name="BoxyRed1SamplingAndParking",group="tests")
+@Disabled
+public class BoxHStrafeToLineRed extends BoxHAutonomousHardwareMap {
+
+    public void runOpMode() throws InterruptedException{
+        telemetry.addData("Status", "Initialized");
+        //initialize the bot
+        telemetry.update();
+
+        init(hardwareMap);
+        int FASTLY = 1;
+
+
+        //wait till start here in the this place
+        waitForStart();
+        // continues to run until you run out of time or hit stop
+        while(opModeIsActive()) {
+
+            // STATE 1
+            strafeLeft(FASTLY * 0.75);
+            sleep(3000);
+            stopMotors();
+            //get reason for idle() later
+            idle();
+        }
+    }
+}
