323fca084c2d5b8e5f1665f997edfea8f4681d56
Robert Long <robertlong1225@gmail.com>
2019-11-09 11:47:30 -0500

    Autonomous for all starting positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BoxHRedBuildZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BoxHRedBuildZone.java
@@ -9,23 +9,31 @@ public class BoxHRedBuildZone extends BoxHAutonomousHardwareMap {
         int state = 0;
         if (state == 0){
             //init robot
-
+            init(hardwareMap);
+            waitForStart();
             state = 1;
         }
 
         if (state == 1){
             //move foreword 1 inch
-
+            telemetry.addData("State","1");
+            telemetry.update();
+            encoderDrive(.5 , 1,1,1,1,5);
             state = 2;
         }
 
         if (state == 2) {
+            telemetry.addData("State","2");
+            telemetry.update();
             //strafe left 30 inches
-
+            strafeLeftEncoder(.5,30,20);
             state = 3;
         }
 
         if (state == 3) {
+            telemetry.addData("State","3");
+            telemetry.update();
+            stopMotors();
             //stop!
         }
     }
