ebc183614fc13908efd912f4f7830f0fe1e0537e
chelsea camacho <45580961+byt3m1@users.noreply.github.com>
2019-11-20 22:03:39 -0500

    increased the power for liftMotor and clawMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/G9F9TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/G9F9TestDrive.java
@@ -76,28 +76,27 @@ public class G9F9TestDrive extends G9F9TeleOpHandler {
             goForward(0.5);
         } else if(dpadDown){
             goBackward(0.5);
-        } else{
-            stopMotors();
+        } else {
+
+
+            rotationPower = -lStickX * ROTATION_CONSTANT;
+            mag1 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(totalAngle + Math.PI / 4));
+            mag2 = Math.sqrt(Math.pow(rStickX, 2) + Math.pow(rStickY, 2)) * (Math.sin(totalAngle - Math.PI / 4));
+
+            maxPower = Math.max(Math.abs(mag1) + Math.abs(rotationPower), Math.abs(mag2) + Math.abs(rotationPower));
+
+
+            if (maxPower > 1.0)
+                scaleDown = 1.0 / maxPower;
+            else
+                scaleDown = 1.0;
+
+
+            leftFront.setPower((mag2 - rotationPower) * scaleDown);
+            leftBack.setPower((mag1 - rotationPower) * scaleDown);
+            rightBack.setPower((mag2 + rotationPower) * scaleDown);
+            rightFront.setPower((mag1 + rotationPower) * scaleDown);
         }
-
-        rotationPower = -lStickX * ROTATION_CONSTANT;
-        mag1 = Math.sqrt(Math.pow(rStickX,2) + Math.pow(rStickY,2)) * (Math.sin(totalAngle + Math.PI / 4));
-        mag2 = Math.sqrt(Math.pow(rStickX,2) + Math.pow(rStickY,2)) * (Math.sin(totalAngle - Math.PI / 4));
-
-        maxPower = Math.max(Math.abs(mag1) +  Math.abs(rotationPower) , Math.abs(mag2) +  Math.abs(rotationPower));
-
-
-        if (maxPower > 1.0)
-            scaleDown = 1.0 / maxPower;
-        else
-            scaleDown = 1.0;
-
-
-        leftFront.setPower((mag2 - rotationPower) * scaleDown);
-        leftBack.setPower((mag1 - rotationPower) * scaleDown);
-        rightBack.setPower((mag2 + rotationPower) * scaleDown );
-        rightFront.setPower((mag1 + rotationPower) * scaleDown);
-
     }
 
     public void handleGamepad2(Gamepad gamepad){
@@ -112,10 +111,10 @@ public class G9F9TestDrive extends G9F9TeleOpHandler {
         bPress = gamepad.b;
 
         if (dpadUp && liftMotor.getCurrentPosition() < LIFT_MAX_POS){
-            liftMotor.setPower(0.4);
+            liftMotor.setPower(1);
             //down
         }else if (dpadDown && liftMotor.getCurrentPosition() > LIFT_MIN_POS){
-            liftMotor.setPower(-0.4);
+            liftMotor.setPower(-1);
             //up
 
         }else{
@@ -124,9 +123,9 @@ public class G9F9TestDrive extends G9F9TeleOpHandler {
 
         //see above comment
         if (aPress && clawMotor.getCurrentPosition() < CLAW_MIN_POS){
-            clawMotor.setPower(0.4);
+            clawMotor.setPower(1);
         }else if (bPress && clawMotor.getCurrentPosition() > CLAW_MAX_POS){
-            clawMotor.setPower(-0.4);
+            clawMotor.setPower(-1);
         }else {
             clawMotor.setPower(0);
             //stops(!) claw
