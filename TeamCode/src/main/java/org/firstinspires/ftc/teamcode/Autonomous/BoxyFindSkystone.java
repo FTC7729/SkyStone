49153fb8335539b9f160c2247790444dc5dbccba
Robert Long <robertlong1225@gmail.com>
2020-01-14 19:29:32 -0500

    minor change to boxyfindskystone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BoxyFindSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BoxyFindSkystone.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
@@ -296,6 +297,22 @@ public class BoxyFindSkystone extends BoxHAutonomousHardwareMap{
                         break;
                     }
                 }
+
+                // Provide feedback as to where the robot is located (if we know).
+                if (targetVisible) {
+                    // express position (translation) of robot in inches.
+                    VectorF translation = lastLocation.getTranslation();
+                    telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                            translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+                    // express the rotation of the robot in degrees.
+                    Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                    telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+                }
+                else {
+                    telemetry.addData("Visible Target", "none");
+                }
+                telemetry.update();
             }
         }
 }
