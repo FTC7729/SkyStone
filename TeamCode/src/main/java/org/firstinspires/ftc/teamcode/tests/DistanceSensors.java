b3c2d409c83b0c345f85a16822cccb62fae47d4f
Robert Long <robertlong1225@gmail.com>
2020-02-08 11:19:03 -0500

    minor fixes to distance sensor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DistanceSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DistanceSensors.java
@@ -6,8 +6,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Autonomous.G9F9AutonomousHardwareMap;
 @Autonomous
 public class DistanceSensors extends G9F9AutonomousHardwareMap {
+
     public void runOpMode (){
-        while (true){
+        waitForStart();
+        while (opModeIsActive()){
             //doesnt work (yet!)
             telemetry.addData("right range", String.format("%.01f in", rightDistanceSensor.getDistance(DistanceUnit.INCH)));
             telemetry.addData("left range", String.format("%.01f in", leftDistanceSensor.getDistance(DistanceUnit.INCH)));
